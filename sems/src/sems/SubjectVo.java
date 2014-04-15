6e4af8894f5ef4543ebaba69d6923b305811e077
Administrator <Administrator@MSDN-SPECIAL>
2014-04-15 16:34:57 +0900

    .

---
+++ sems/src/sems/SubjectVo.java
@@ -0,0 +1,37 @@
+package sems;
+
+//	setter / getter 적용 <= 캡슐화
+
+public class SubjectVo {
+	private int sno;
+	private String title;   
+	private String description;
+
+	public int getSno() {
+		return sno;
+	}
+	
+	public SubjectVo setSno(int sno) {
+		this.sno = sno;
+		return this;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public SubjectVo setTitle(String title) {
+		this.title = title;
+		return this;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public SubjectVo setDescription(String description) {
+		this.description = description;
+		return this;
+	}
+}
+
---
+++ sems/src/sems/update.java
@@ -0,0 +1,40 @@
+package sems;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/subject/update.bit")
+public class update extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		MysqlSubjectDao dao = new MysqlSubjectDao();
+
+		int sno = Integer.parseInt(request.getParameter("no"));
+		String title = request.getParameter("title");
+		String description = request.getParameter("description");
+		SubjectVo subjectVo = new SubjectVo().setSno(sno).setTitle(title).setDescription(description);
+
+		try {
+			dao.update(subjectVo);
+			response.setContentType("text/html;charset=UTF-8");
+
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>test01</title><style>td{border : 1px solid red; text-align : center;} .no{width : 40px;} .subject{width : 200px;}</style></head>");
+			out.println("<body>");
+			out.println("<h1>변경성공입니다.</h1>");
+			out.println("</body></html>");
+		} catch (Throwable e) {
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<h1>변경실패입니다.</h1>");
+		}
+	}
+}
