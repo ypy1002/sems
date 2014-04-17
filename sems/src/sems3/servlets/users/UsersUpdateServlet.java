7690a590bbe177a9459af0473ec441a4148fbc74
hihihello0101 <hihihello0101@gmail.com>
2014-04-17 20:26:24 +0900

    ;;

--- sems/src/sems3/servlets/users/UsersUpdateServlet.java
+++ sems/src/sems3/servlets/users/UsersUpdateServlet.java
@@ -4,6 +4,7 @@ package sems3.servlets.users;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,9 +21,6 @@ public class UsersUpdateServlet extends HttpServlet {
 	protected void doGet(
 			HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-				
 		
 		try {
 			//1) DB에서 과목 상세 정보를 가져온다.
@@ -30,73 +28,13 @@ public class UsersUpdateServlet extends HttpServlet {
 			UsersDao dao = (UsersDao)this.getServletContext()
 					.getAttribute("usersDao");
 			UsersVo vo = dao.detail(no);
-			
-			out.println("<!DOCTYPE html><html><head><meta charset='UTF-8'><title>사용자 데이터 입력 폼</title>");
-			out.println("<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>"
-
-			+ "<style>"
-			
-			  + "body { width : 100%; height : 100%; background-color : black;}"
-			  + "input { border-radius : 5px;}"
-			  + "a { text-decoration : none; color : yellow;}"
-			  + "div { width : 50%; margin-left : 24%; text-align : center; margin-left : 22%;}"
-			  + "h1 { text-align : center; color : gray; }"
-			  
-			  + "</style>");
-			
-			out.println("</head><body><div><h1>사용자 데이터 변경</h1>");
-			out.println("<form action='/sems/users/update.bit' method='post' onsubmit='return re();'>");
-			out.println("<input type='text' name='uno' value='" + vo.getUno() +"' readonly><br>");
-			out.println("<input type='text' id='email' name='email' placeholder='Email' value='" + vo.getEmail() +"'><br>");
-			out.println("<input type='password' id='password' name='password' placeholder='password' value='" + vo.getPassword() +"'><br>");
-			out.println("<input type='text' id='name' name='name' placeholder='name' value='" + vo.getName() +"'><br>");
-			out.println("<input type='text' id='tel' name='tel' placeholder='tel' value='" + vo.getTel() +"'><br>");
-			out.println("<input type='text' name='fax' placeholder='fax' value='" + vo.getFax() +"'><br>");
-			out.println("<input type='text' name='postNo' placeholder='postNo' value='" + vo.getPostNo() +"'><br>");
-			out.println("<input type='text' name='addr' placeholder='addr' value='" + vo.getAddr() +"'><br>");
-			out.println("<input type='text' name='photoPath' placeholder='photoPath' value='" + vo.getPhoto() +"'><br><br>");
-			out.println("<input type='submit' value='변경' onclick=check();>");
-			out.println("<input type='reset' value='취소'><br><br>"
-					+ "<a href='list.bit?pageNo=1&pageSize=10'>List</a>");
-			out.println("</form></div>");
-			
-			out.println("<script>");
-
-			out.println("var i = 0;");
-			
-			out.println("	function check(){");
-			out.println("  	if($('#email').val() == ''){");
-			out.println("    	alert('email은 필수 입력 항목입니다.');");
-			out.println("   	i++;");
-			out.println("  	}else if($('#password').val() == ''){");
-			out.println("    	alert('password는 필수 입력 항목입니다.');");
-			out.println("   	i++;");
-			out.println("  	}else if($('#name').val() == ''){");
-			out.println("    	alert('name은 필수 입력 항목입니다.');");
-			out.println("   	i++;");
-			out.println("   }else if($('#tel').val() == ''){");
-			out.println("   	alert('tel은 필수 입력 항목입니다.');");
-			out.println("   	i++;");
-			out.println("  	}");
-			out.println(" }");
-
-			out.println("	function re(){");
-			out.println("		if(i == 0){");
-			out.println("			return true;");
-			out.println("		}else{");
-			out.println("			i = 0;");
-			out.println("			return false;");
-			out.println("		}");
-			out.println("	}");
-
-			out.println("</script>");
-			out.println("</body></html>");
-			
+			request.setAttribute("vo", vo);
+			RequestDispatcher rd =
+					request.getRequestDispatcher("/users/update.jsp");
+			rd.forward(request, response);
 		} catch (Throwable e) {
-			out.println("오류 발생 했음!");
 			e.printStackTrace();
 		}
-		out.println("</body></html>");
 	}
 	
 	@Override
@@ -105,13 +43,7 @@ public class UsersUpdateServlet extends HttpServlet {
 	    throws ServletException, IOException {
 		// CharacterEncodingFilter로 대체함.
 		//request.setCharacterEncoding("UTF-8");
-		
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("<html><head><title>과목변경</title></head><body>");
-		
 		try {
-			out.println("<h1>과목 변경 결과</h1>");
 			
 			UsersDao dao = (UsersDao)this.getServletContext()
 					.getAttribute("usersDao");
@@ -129,15 +61,12 @@ public class UsersUpdateServlet extends HttpServlet {
 			
 			dao.update(vo);
 			
-			out.println("변경 성공!");
 			
 			response.sendRedirect("detail.bit?no=" + vo.getUno());
 			
 		} catch (Throwable e) {
-			out.println("<h1>오류 발생 했음!</h1>");
 			e.printStackTrace();
 		}
-		out.println("</body></html>");
 	}
 }
 
