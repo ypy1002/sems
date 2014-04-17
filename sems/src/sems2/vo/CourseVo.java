829aed4e329f5afb1206a4fd78deb51ccd92a344
유형덕 <ypy1002@gmail.com>
2014-04-17 17:32:35 +0900

    e

--- sems/src/sems2/vo/CourseVo.java
+++ sems/src/sems2/vo/CourseVo.java
@@ -1,5 +1,6 @@
 package sems2.vo;
 
+
 //	setter / getter 적용 <= 캡슐화
 
 public class CourseVo {
@@ -8,39 +9,33 @@ public class CourseVo {
 	private String description;
 	private int hours;
 	
-	public int getHours() {
-		return hours;
-	}
-
-	public CourseVo setHours(int hours) {
-		this.hours = hours;
-		return this;
-	}
-
+	
 	public int getCno() {
 		return cno;
 	}
-	
 	public CourseVo setCno(int cno) {
 		this.cno = cno;
 		return this;
 	}
-	
 	public String getTitle() {
 		return title;
 	}
-	
 	public CourseVo setTitle(String title) {
 		this.title = title;
 		return this;
 	}
-	
 	public String getDescription() {
 		return description;
 	}
-	
 	public CourseVo setDescription(String description) {
 		this.description = description;
+		return this;
+	}
+	public int getHours() {
+		return hours;
+	}
+	public CourseVo setHours(int hours) {
+		this.hours = hours;
 		return this;
 	}
 }
